425bddbe2e35bd9d9e3924b07403112f862657bb
pujitha Medikonda <pujitha.medikonda@smscountry.com>
2018-03-02 12:17:34 +0530

    CHnages by pujitha

--- TestClient/Program.cs
+++ TestClient/Program.cs
@@ -21,7 +21,9 @@ namespace TestClient
                 Dictionary<string, TablePreferences> prefs = new Dictionary<string, TablePreferences>();
                 //prefs.Add("InputTypes", new TablePreferences(RootName: "FieldTypes", childElementNameForRows: "FieldType", columnValuesAsXmlAttributes: true, singleRowAsSingleEntity: true));                
                 //dynamic d = c.CreateQuotation(1, 1, 1, 1, "{'Balance':{'Id':'1','Occurance':'1','Amount':'1500','PPPS':'1','PromotionalPulse/Voi':'1','TPPS':'1','TransactionalPulse/V':'1','ValidityInDays':'15','ExtraCharges':[{'Description':'Additional','Amount':'15'}]}}", "", 1);
-                dynamic d = c.CreateQuotation(1, 3, 1, 1, "{'Balance':{'Id':'1','Occurance':'1','Amount':'1500','PPPS':'1','PromotionalPulse/Voi':'1','TPPS':'1','TransactionalPulse/V':'1','ValidityInDays':'15','ExtraCharges':[{'Description':'Additional','Amount':'15'}]}}", "::1", 1);
+                //dynamic d = c.GetQuotationDetails(4026, false);
+               dynamic d1 = c.GetQuotationDetails(4029, false);
+
                 //dynamic d = c.CreateQuotation(1, 1, 1, 1, "{'Balance':[{'Id':'1','Occurance':'1','Amount': '100','ValidityInDays': '10','TPPS': '10','PPPS': '10','TransactionalPulse/V': '','PromotionalPulse/Voi': '','ExtraCharges' : [{'Description': 'raja','Amount': '109'}]},{'Id':'2','Occurance':'2','Amount': '500','ValidityInDays': '50','TPPS': '50','PPPS': '50','TransactionalPulse/V': '','PromotionalPulse/Voi': '','ExtraCharges' : [{'Description': 'raja','Amount': '8909'}]}],'SenderName':{'Id':'1','Occurance':'1','SetupCharge': '0','Name': 'Tez','Price': '1000','ValidityInDays': '30','MonthlyRental': '1000'}}", "", 1);
                 //dynamic d1 = c.GetServices(productId: 1, includeServiceProperties: true);
                 //dynamic d = c.GetServiceProperties(1, true, null);
