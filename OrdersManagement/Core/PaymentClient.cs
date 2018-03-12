c7d77d1858e80feae39af7e917d405e3ecf811f8
pujitha Medikonda <pujitha.medikonda@smscountry.com>
2018-03-12 21:16:13 +0530

    Verifypayment statuses

--- OrdersManagement/Core/PaymentClient.cs
+++ OrdersManagement/Core/PaymentClient.cs
@@ -58,7 +58,7 @@ namespace OrdersManagement.Core
                 this._helper.CreateProperty(Label.MESSAGE, this._sqlCommand.GetMessage());
             return this._helper.GetResponse();
         }
-        
+
 
         #endregion
 
@@ -171,18 +171,18 @@ namespace OrdersManagement.Core
                 this.Clean();
             }
         }
-       
 
-        public dynamic CreatePayment(int productId, int accountId, int employeeId, int invoiceId,int billingModeId, 
-            int paymentGatewayId, float paymentAmount, int bankAccountId, DateTime depositeDate, int activatePercentage, 
-            string comments,bool isTDSApplicable,int tdsPercentage,string chequeNumber,string attachments,string transactionNumber,
-            string clientAccountNumber,string clientAccountName,string clientBankName,string clientBankBranch,
-            int onlinePaymentGatewayId,string paymentGatewayReferenceId,
+
+        public dynamic CreatePayment(int productId, int accountId, int employeeId, int invoiceId, int billingModeId,
+            int paymentGatewayId, float paymentAmount, int bankAccountId, DateTime depositeDate, int activatePercentage,
+            string comments, bool isTDSApplicable, int tdsPercentage, string chequeNumber, string attachments, string transactionNumber,
+            string clientAccountNumber, string clientAccountName, string clientBankName, string clientBankBranch,
+            int onlinePaymentGatewayId, string paymentGatewayReferenceId,
             Dictionary<string, TablePreferences> tablePreferences = null)
         {
             try
             {
-    
+
                 this._sqlCommand = new SqlCommand(StoredProcedure.CREATE_PAYMENT, this._sqlConnection);
                 this._sqlCommand.Parameters.Add(ProcedureParameter.ACCOUNT_ID, SqlDbType.Int).Value = accountId;
                 this._sqlCommand.Parameters.Add(ProcedureParameter.PRODUCT_ID, SqlDbType.Int).Value = productId;
@@ -197,15 +197,15 @@ namespace OrdersManagement.Core
                 this._sqlCommand.Parameters.Add(ProcedureParameter.COMMENTS, SqlDbType.VarChar, -1).Value = comments;
                 this._sqlCommand.Parameters.Add(ProcedureParameter.IS_TDS_APPLICABLE, SqlDbType.Bit).Value = isTDSApplicable;
                 this._sqlCommand.Parameters.Add(ProcedureParameter.TDS_Percentage, SqlDbType.Int).Value = tdsPercentage;
-                this._sqlCommand.Parameters.Add(ProcedureParameter.ATTACHMENTS, SqlDbType.VarChar,1000).Value = attachments;
-                this._sqlCommand.Parameters.Add(ProcedureParameter.CHEQUE_NUMBER, SqlDbType.VarChar,64).Value = chequeNumber;
-                this._sqlCommand.Parameters.Add(ProcedureParameter.TRANSACTION_NUMBER, SqlDbType.VarChar,50).Value = transactionNumber;
-                this._sqlCommand.Parameters.Add(ProcedureParameter.CLIENT_ACCOUNT_NUMBER, SqlDbType.VarChar,32).Value = clientAccountNumber;
-                this._sqlCommand.Parameters.Add(ProcedureParameter.CLIENT_ACCOUNT_NAME, SqlDbType.VarChar,128).Value = clientAccountName;
-                this._sqlCommand.Parameters.Add(ProcedureParameter.CLIENT_BANK_NAME, SqlDbType.VarChar,64).Value = clientBankName;
-                this._sqlCommand.Parameters.Add(ProcedureParameter.CLIENT_BANK_BRANCH, SqlDbType.VarChar,64).Value = clientBankBranch;
+                this._sqlCommand.Parameters.Add(ProcedureParameter.ATTACHMENTS, SqlDbType.VarChar, 1000).Value = attachments;
+                this._sqlCommand.Parameters.Add(ProcedureParameter.CHEQUE_NUMBER, SqlDbType.VarChar, 64).Value = chequeNumber;
+                this._sqlCommand.Parameters.Add(ProcedureParameter.TRANSACTION_NUMBER, SqlDbType.VarChar, 50).Value = transactionNumber;
+                this._sqlCommand.Parameters.Add(ProcedureParameter.CLIENT_ACCOUNT_NUMBER, SqlDbType.VarChar, 32).Value = clientAccountNumber;
+                this._sqlCommand.Parameters.Add(ProcedureParameter.CLIENT_ACCOUNT_NAME, SqlDbType.VarChar, 128).Value = clientAccountName;
+                this._sqlCommand.Parameters.Add(ProcedureParameter.CLIENT_BANK_NAME, SqlDbType.VarChar, 64).Value = clientBankName;
+                this._sqlCommand.Parameters.Add(ProcedureParameter.CLIENT_BANK_BRANCH, SqlDbType.VarChar, 64).Value = clientBankBranch;
                 this._sqlCommand.Parameters.Add(ProcedureParameter.ONLINE_PAYMENT_GATEWAY_ID, SqlDbType.TinyInt).Value = onlinePaymentGatewayId;
-                this._sqlCommand.Parameters.Add(ProcedureParameter.PAYMENT_GATEWAY_REFERENCE_ID, SqlDbType.VarChar,64).Value = paymentGatewayReferenceId;
+                this._sqlCommand.Parameters.Add(ProcedureParameter.PAYMENT_GATEWAY_REFERENCE_ID, SqlDbType.VarChar, 64).Value = paymentGatewayReferenceId;
                 this._helper.PopulateCommonOutputParameters(ref this._sqlCommand);
                 this._da = new SqlDataAdapter(this._sqlCommand);
                 this._da.Fill(this._ds = new DataSet());
@@ -226,18 +226,18 @@ namespace OrdersManagement.Core
             }
         }
 
-        internal dynamic GetPayments(byte productId,int accountId,string mobile,string email,int paymentStatus,
-            string number,byte billingMode,DateTime fromDateTime,DateTime toDateTime, Dictionary<string, TablePreferences> tablePreferences = null)
+        internal dynamic GetPayments(byte productId, int accountId, string mobile, string email, int paymentStatus,
+            string number, byte billingMode, DateTime fromDateTime, DateTime toDateTime, Dictionary<string, TablePreferences> tablePreferences = null)
         {
             try
             {
                 this._sqlCommand = new SqlCommand(StoredProcedure.GET_PAYMENTS, this._sqlConnection);
-                this._sqlCommand.Parameters.Add(ProcedureParameter.PRODUCT_ID , SqlDbType.TinyInt).Value = productId;
-                this._sqlCommand.Parameters.Add(ProcedureParameter.ACCOUNT_ID , SqlDbType.Int).Value = accountId;
-                this._sqlCommand.Parameters.Add(ProcedureParameter.MOBILE, SqlDbType.VarChar,15).Value = mobile;
+                this._sqlCommand.Parameters.Add(ProcedureParameter.PRODUCT_ID, SqlDbType.TinyInt).Value = productId;
+                this._sqlCommand.Parameters.Add(ProcedureParameter.ACCOUNT_ID, SqlDbType.Int).Value = accountId;
+                this._sqlCommand.Parameters.Add(ProcedureParameter.MOBILE, SqlDbType.VarChar, 15).Value = mobile;
                 this._sqlCommand.Parameters.Add(ProcedureParameter.EMAIL, SqlDbType.VarChar, 126).Value = email;
-                this._sqlCommand.Parameters.Add(ProcedureParameter.PAYMENT_STATUS , SqlDbType.TinyInt).Value = paymentStatus;
-                this._sqlCommand.Parameters.Add(ProcedureParameter.NUMBER, SqlDbType.VarChar,32).Value = number;
+                this._sqlCommand.Parameters.Add(ProcedureParameter.PAYMENT_STATUS, SqlDbType.TinyInt).Value = paymentStatus;
+                this._sqlCommand.Parameters.Add(ProcedureParameter.NUMBER, SqlDbType.VarChar, 32).Value = number;
                 this._sqlCommand.Parameters.Add(ProcedureParameter.BILLING_MODE_ID, SqlDbType.Int).Value = billingMode;
                 this._sqlCommand.Parameters.Add(ProcedureParameter.FROM_DATE_TIME, SqlDbType.DateTime).Value = fromDateTime;
                 this._sqlCommand.Parameters.Add(ProcedureParameter.TO_DATE_TIME, SqlDbType.DateTime).Value = toDateTime;
@@ -264,7 +264,7 @@ namespace OrdersManagement.Core
         }
 
 
-        internal dynamic GetPaymentDetails(byte productId,int orderId, Dictionary<string, TablePreferences> tablePreferences = null)
+        internal dynamic GetPaymentDetails(byte productId, int orderId, Dictionary<string, TablePreferences> tablePreferences = null)
         {
             try
             {
@@ -292,7 +292,32 @@ namespace OrdersManagement.Core
                 this.Clean();
             }
         }
-        
+        internal dynamic VerifyPaymentStatuses(long orderId, Dictionary<string, TablePreferences> tablePreferences = null)
+        {
+            try
+            {
+                this._sqlCommand = new SqlCommand(StoredProcedure.VERIFY_PAYMENT_STATUSES, this._sqlConnection);
+                this._sqlCommand.Parameters.Add(ProcedureParameter.ORDER_ID, SqlDbType.BigInt).Value = orderId;
+                this._helper.PopulateCommonOutputParameters(ref this._sqlCommand);
+                this._da = new SqlDataAdapter(this._sqlCommand);
+                this._da.Fill(this._ds = new DataSet());
+                if (!this._sqlCommand.IsSuccess())
+                    return this.ErrorResponse();
+                this._ds.Tables.Add(this._helper.ConvertOutputParametersToDataTable(this._sqlCommand.Parameters));
+                this._helper.ParseDataSet(this._ds, tablePreferences);
+                return this._helper.GetResponse();
+            }
+            catch (Exception e)
+            {
+                Logger.Error(string.Format("Unable to verify  VerifyPaymentStatus. {0}", e.ToString()));
+                throw new QuotationException(string.Format("nable to verify  VerifyPaymentStatus. {0}", e.Message));
+            }
+            finally
+            {
+                this.Clean();
+            }
+        }
+
         #endregion
     }
 }
